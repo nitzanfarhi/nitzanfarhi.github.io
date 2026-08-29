1ddf07b36db515b49eb39ca5de66a2649504bdc8
Nitzan Farhi <nitzan@kidslearning.app>
2026-08-29 22:11:23 +0300

    feat(kids): update kids game build 2026-08-29 22:11:23

--- kids/sw.js
+++ kids/sw.js
@@ -7,7 +7,7 @@
  * - Stale-While-Revalidate for app shell code (HTML/JS/CSS)
  */
 
-const CACHE_NAME = 'kids-learn-cache-v5';
+const CACHE_NAME = 'kids-learn-cache-v6';
 
 const PRECACHE_ASSETS = [
   './',
